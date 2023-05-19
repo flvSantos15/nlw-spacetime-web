2cbb3b08a03ca775aed700e1fe5fa89f1ab29d6a
flvSantos15 <flvSantos300@gmail.com>
2023-05-18 21:24:39 -0300

    feat: new memories page

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,12 @@
+import { api } from '@/lib/api'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  const redirectUrl = new URL('/', req.url)
+
+  return NextResponse.redirect(redirectUrl, {
+    headers: {
+      'Set-Cookie': `token=; Path=/; max-age=0`
+    }
+  })
+}
